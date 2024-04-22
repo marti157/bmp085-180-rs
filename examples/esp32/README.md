de2c781a5ba38be5dcbe7c24d1fd143da7e43ac9
marti157 <31134295+marti157@users.noreply.github.com>
2024-04-22 09:47:21 +0200

    Update esp-hal for example, add esp_println

---
+++ examples/esp32/README.md
@@ -0,0 +1,5 @@
+## ESP32 Example
+
+Refer to [ESP-rs book, Xtensa Target](https://docs.esp-rs.org/book/installation/riscv-and-xtensa.html), as the `esp` toolchain needs to be installed.
+
+`$ cargo run --release`
--- examples/esp32/src/main.rs
+++ examples/esp32/src/main.rs
@@ -1,23 +1,32 @@
 #![no_std]
 #![no_main]
-#![feature(type_alias_impl_trait)]
 
-use esp32c3_hal::{clock::ClockControl, i2c::I2C, peripherals::Peripherals, prelude::*, IO};
 use esp_backtrace as _;
+use esp_hal::{clock::ClockControl, delay::Delay, gpio::IO, i2c::I2C, peripherals::Peripherals, prelude::*};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let mut i2c0 = I2C::new(
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let delay = Delay::new(&clocks);
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let mut i2c = I2C::new(
         peripherals.I2C0,
         io.pins.gpio3,
         io.pins.gpio2,
-        100u32.kHz(),
+        100.kHz(),
         &clocks,
+        None,
     );
 
-    loop {}
+    esp_println::logger::init_logger_from_env();
+
+    loop {
+        log::info!("Hello world!");
+        delay.delay(500.millis());
+    }
 }
