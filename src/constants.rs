63467f1849fd92e7bc8d0bb08b1954b798240ec0
marti157 <31134295+marti157@users.noreply.github.com>
2024-04-25 14:28:49 +0200

    Add constants file

---
+++ src/constants.rs
@@ -0,0 +1,2 @@
+pub const BMP180_DEVICE_ADDR: u8 = 0x77;
+pub const BMP180_ID_REG: u8 = 0xD0;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+mod constants;
+use constants::{BMP180_DEVICE_ADDR, BMP180_ID_REG};
 use embedded_hal::i2c::I2c;
 
 pub struct BMP180<I2C> {
@@ -12,12 +14,16 @@ where
     I2C: I2c,
 {
     pub fn new(i2c: I2C) -> Self {
-        Self { i2c, address: 0x77 }
+        Self {
+            i2c,
+            address: BMP180_DEVICE_ADDR,
+        }
     }
 
     pub fn read_id(&mut self) -> Result<u8, I2C::Error> {
         let mut id = [0];
-        self.i2c.write_read(self.address, &[0xD0], &mut id)?;
+        self.i2c
+            .write_read(self.address, &[BMP180_ID_REG], &mut id)?;
         Ok(id[0])
     }
 }
