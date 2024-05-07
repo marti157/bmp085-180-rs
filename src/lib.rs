0b3a05a054a3f1860a2bdef7bec9c933ae5efa9a
marti157 <31134295+marti157@users.noreply.github.com>
2024-05-07 18:19:56 +0200

    Fix some logic when calculating pressure

--- src/lib.rs
+++ src/lib.rs
@@ -164,7 +164,7 @@ where
         self.i2c
             .write_read(self.address, &[BMP180_OUT_LSB_REG], &mut rx_buffer[1..2])?;
         self.i2c
-            .write_read(self.address, &[BMP180_OUT_XLSB_REG], &mut rx_buffer[3..4])?;
+            .write_read(self.address, &[BMP180_OUT_XLSB_REG], &mut rx_buffer[2..3])?;
         let up =
             (((rx_buffer[0] as i32) << 16) + ((rx_buffer[1] as i32) << 8) + (rx_buffer[0] as i32))
                 >> (8 - self.oss);
@@ -201,23 +201,23 @@ where
     fn calculate_pressure(&self, b5: i32, up: i32) -> i32 {
         let b6: i32 = b5 as i32 - 4000;
         let x1 = self.calib_data.b2 as i32 * ((b6 * b6) >> 12) >> 11;
-        let x2 = self.calib_data.ac2 as i32 * (b6 >> 11);
+        let x2 = (self.calib_data.ac2 as i32 * b6) >> 11;
         let x3 = x1 + x2;
         let b3 = (((self.calib_data.ac1 as i32 * 4 + x3) >> self.oss) + 2) / 4;
-        let mut x1 = self.calib_data.ac3 as i32 * (b6 >> 13);
-        let mut x2 = self.calib_data.b2 as i32 * ((b6 * b6) >> 12) >> 16;
-        let x3 = (x1 + x2 + x3) >> 2;
+        let mut x1 = (self.calib_data.ac3 as i32 * b6) >> 13;
+        let mut x2 = self.calib_data.b1 as i32 * ((b6 * b6) >> 12) >> 16;
+        let x3 = (x1 + x2 + 2) >> 2;
         let b4 = (self.calib_data.ac4 as u32 * (x3 as u32 + 0x8000)) >> 15;
         let b7 = (up as u32 - b3 as u32) * (50_000 >> self.oss);
         let p = if b7 < 0x80000000 {
-            (b7 as i32 * 2) / b4 as i32
+            ((b7 as i64 * 2) / b4 as i64) as i32
         } else {
             (b7 as i32 / b4 as i32) * 2
         };
         x1 = (p >> 8) * (p >> 8);
         x1 = (x1 * 3038) >> 16;
         x2 = (-7357 * p) >> 16;
-        let pressure = p + (x1 + x2 + 3791) >> 4;
+        let pressure = p + ((x1 + x2 + 3791) >> 4);
 
         pressure
     }
