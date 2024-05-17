7fd4169654a99907f670f61bede7d481ce5f9693
marti157 <31134295+marti157@users.noreply.github.com>
2024-05-17 21:03:00 +0200

    Add integration tests for test_connection method

---
+++ tests/integration.rs
@@ -0,0 +1,38 @@
+use bmp085_180_rs::BMP;
+use embedded_hal::i2c::ErrorKind;
+use embedded_hal_mock::eh1::delay::NoopDelay;
+use embedded_hal_mock::eh1::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
+
+#[test]
+fn test_connection_ok_with_valid_id() {
+    let expectations = [I2cTransaction::write_read(0x77, vec![0xD0], vec![0x55])];
+    let mut i2c = I2cMock::new(&expectations);
+
+    let mut bmp = BMP::new(i2c.clone(), NoopDelay, Default::default());
+
+    assert_eq!(bmp.test_connection(), Ok(()));
+    i2c.done();
+}
+
+#[test]
+fn test_connection_fails_with_invalid_id() {
+    let expectations = [I2cTransaction::write_read(0x77, vec![0xD0], vec![0xFF])];
+    let mut i2c = I2cMock::new(&expectations);
+
+    let mut bmp = BMP::new(i2c.clone(), NoopDelay, Default::default());
+
+    assert_eq!(bmp.test_connection(), Err("Unrecognized device identifier"));
+    i2c.done();
+}
+
+#[test]
+fn test_connection_fails_if_i2c_error() {
+    let expectations =
+        [I2cTransaction::write_read(0x77, vec![0xD0], vec![0xFF]).with_error(ErrorKind::Other)];
+    let mut i2c = I2cMock::new(&expectations);
+
+    let mut bmp = BMP::new(i2c.clone(), NoopDelay, Default::default());
+
+    assert_eq!(bmp.test_connection(), Err("I2C error"));
+    i2c.done();
+}
